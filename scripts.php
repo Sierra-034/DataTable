840690a98be1963fbdf62fa44e030154a059f05b
Samuel Gómez Balderas <sierra_034@outlook.com>
2018-12-02 00:26:43 -0600

    Estructura de carpetas y dependencias

---
+++ scripts.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" type="text/css" href="librerias/boostrap/boostrap.min.css">
+<link rel="stylesheet" type="text/css" href="librerias/datatable/boostrap.css">
+<link rel="stylesheet" type="text/css" href="librerias/datatable/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" type="text/css" href="librerias/alertify/css/alertify.css">
+<link rel="stylesheet" type="text/css" href="librerias/alertify/css/themes/bootstrap.css">
+
+<script type="text/javascript" src="librerias/jquery.min.js"></script>
+<script type="text/javascript" src="librerias/bootstrap/popper.min.js"></script>
+<script type="text/javascript" src="librerias/bootstrap/bootstrap.min.js"></script>
+<script type="text/javascript" src="librerias/datatable/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="librerias/datatable/dataTables.bootstrap4.min.js"></script>
+<script type="text/javascript" src="librerias/alertify/alertify.js"></script>
